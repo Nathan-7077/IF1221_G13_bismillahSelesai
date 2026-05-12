1d1a328a57dfb2fc4c22973d609ffecd63ff67e1
Fawaaz <rauuffawaaz@gmail.com>
2026-05-12 20:03:15 +0700

    Update : Membuat fakta pakai list

--- src/card.pl
+++ src/card.pl
@@ -1,21 +1,5 @@
-warna(merah).
-warna(biru).
-warna(hijau).
-warna(kuning).
-jenis(0).
-jenis(1).
-jenis(2).
-jenis(3).
-jenis(4).
-jenis(5).
-jenis(6).
-jenis(7).
-jenis(8).
-jenis(9).
-jenis(skip).
-jenis(reverse).
-jenis(draw_two).
-
-kartu(wild, draw_four).
 kartu(wild, _).
 kartu(X, Y) :- warna(X), jenis(Y).
+
+warna(['Merah','Biru','Hijau','Kuning']).
+jenis([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'skip', 'reverse', 'draw_two', 'draw_four']).
