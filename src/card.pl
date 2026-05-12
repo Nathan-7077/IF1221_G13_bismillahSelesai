31197b0b289db33275b9c03c05f0dc5e4aa2f405
Nathan-7077 <marvelnathaniel07@gmail.com>
2026-05-12 20:36:22 +0700

    Update card.pl

--- src/card.pl
+++ src/card.pl
@@ -1,17 +1,13 @@
 /* data dasar */
-
 warna(merah).
 warna(kuning).
 warna(hijau).
 warna(biru).
-
 jenis(skip).
 jenis(reverse).
 jenis(draw_two).
-
 jenis(wild).
 jenis(wild_draw_four).
-
 jenis(0).
 jenis(1).
 jenis(2).
@@ -25,14 +21,12 @@ jenis(9).
 
 
 /* rule kartu */
-
 % kartu biasa & aksi
 kartu(Warna, Jenis) :-
     warna(Warna),
     jenis(Jenis),
     Jenis \= wild,
     Jenis \= wild_draw_four.
-
 % kartu wild
 kartu(hitam, wild).
 kartu(hitam, wild_draw_four).
