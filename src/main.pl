15ea1321ad71c8721dea5a1a8c0d2750045c0629
Fawaaz <rauuffawaaz@gmail.com>
2026-05-12 18:59:35 +0700

    Update main.pl

--- src/main.pl
+++ src/main.pl
@@ -1,1 +1,18 @@
+:- include('fakta.pl').
+:- include('rule.pl').
+:- include('startGame.pl').
 
+:- initialization(main).
+
+main :-
+    write('\n Ketik "mulaiUNI." untuk memulai permainan UNI. \n >> '),
+    read(Command),
+    (
+        Command = 'mulaiUNI' ->
+        startGame
+        ;
+        Command = 'exit' ->
+        fail
+        ;
+        main
+    ).
