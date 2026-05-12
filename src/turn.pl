c72dd5f5fa57503834fe098d01d70812ae928470
Nathan-7077 <marvelnathaniel07@gmail.com>
2026-05-12 20:04:03 +0700

    Update turn.pl

--- src/turn.pl
+++ src/turn.pl
@@ -1,1 +1,27 @@
-
+kartuValid(kartu(wild, drawFour), kartu(wild, drawFour)):- !, fail.
+kartuValid(kartu(_, drawTwo), kartu(_, drawTwo)):- !, fail.
+kartuValid(kartu(Color,_), kartu(Color,_)):- !.
+kartuValid(kartu(_, Type), kartu(_, Type)):-
+    Type \= wild,
+    Type \= drawFour, !.
+kartuValid(kartu(wild,_), _):- !.
+cekWDF(Hand, kartu(CurrentColor, CurrentType)):-
+    \+(
+        member(kartu(Color, Type), Hand),
+        Color \= wild,
+        (
+            Color=CurrentColor;
+            Type=CurrentType
+        )
+    ).
+bisaDimainkan(Player, Card):-
+    currentPlayer(Player),
+    hand(Player, Hand),
+    member(Card, Hand),
+    discardPile([Top|_]),
+    kartuValid(Card, Top),
+    (
+        Card=kartu(wild, drawFour)->
+        cekWDF(Hand, Top);
+        true
+    ).
