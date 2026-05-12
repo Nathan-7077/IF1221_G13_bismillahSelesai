5012e4e6ecab596d7b613280c128c240313af672
zenyx7 <adityarasyid797@gmail.com>
2026-05-12 20:25:33 +0700

    Update card.pl

--- src/card.pl
+++ src/card.pl
@@ -1,5 +1,29 @@
-kartu(wild, _).
-kartu(X, Y) :- warna(X), jenis(Y).
+/* data dasar */
 
-warna(['Merah','Biru','Hijau','Kuning']).
-jenis([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'skip', 'reverse', 'draw_two', 'draw_four']).
+warna([merah, kuning, hijau, biru]).
+
+jenis_aksi([skip, reverse, draw_two]).
+jenis_wild([wild, wild_draw_four]).
+
+angka([0,1,2,3,4,5,6,7,8,9]).
+
+
+/* rule kartu */
+
+kartu(kartu(Warna, angka, Nilai)) :-
+    warna(ListWarna),
+    member(Warna, ListWarna),
+
+    angka(ListAngka),
+    member(Nilai, ListAngka).
+
+kartu(kartu(Warna, Jenis, none)) :-
+    warna(ListWarna),
+    member(Warna, ListWarna),
+
+    jenis_aksi(ListJenis),
+    member(Jenis, ListJenis).
+
+kartu(kartu(hitam, Jenis, none)) :-
+    jenis_wild(ListWild),
+    member(Jenis, ListWild).
