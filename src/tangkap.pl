28bafaccc615603d4266ce898522f35c2cd0bdfe
Fawaaz <rauuffawaaz@gmail.com>
2026-05-19 17:48:39 +0700

    Create tangkap.pl

---
+++ src/tangkap.pl
@@ -0,0 +1,29 @@
+cekKartuTinggalSatu(Player):-
+	currentPlayer(Player),
+	cards(Player, Hand),
+	getLength(Hand, Length),
+	Length is 1.
+
+cekPlayerNggaUni(Nama, Hasil):-
+	(
+		playerBilangUni(Nama)
+		->
+		Hasil = 0
+		;
+		Hasil = 1
+	).
+
+tangkapPlayer(Player):-
+	(
+		cekKartuTinggalSatu(Player), cekPlayerNggaUni == 1
+		->
+		/* nambah kartu di tangan dari random */
+		write(Nama),
+		write(' ditangkap!, kartu '),
+		write(Nama),
+		write(' bertambah satu').
+		;
+		write('Tidak bisa menangkap '),
+		write(Nama),
+		/* ambilKartu(). */
+	).
