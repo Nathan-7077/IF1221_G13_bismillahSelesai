72aca1754b9ade94c544f49c6e43c6ce3b767d47
Fawaaz <rauuffawaaz@gmail.com>
2026-05-19 20:54:00 +0700

    Update mainkanKartudanUni.pl

--- src/mainkanKartudanUni.pl
+++ src/mainkanKartudanUni.pl
@@ -1,37 +1,76 @@
+:- include('startGameProgres.pl').
+:- include('handleEffectProgres.pl').
+:- include('playerProgres.pl').
+
+:- dynamic(playerBilangUni/1).
+
+getLength([], 0).
+getLength([_|Tail], Length) :-
+	getLength(Tail, TailLength),
+	Length is TailLength + 1.
+
 cekKartuTinggalDua(Player):-
-	getLength([kartu(Warna, Jenis)| Sisa], Length),
+	cards(Player, Hand),
+	getLength(Hand, Length),
 	Length is 2.
 
-  mainkanKartudanUni(NoKartu):-
+mainkanKartudanUni(NoKartu):-
 	currentPlayer(Player),
 	(
 		cekKartuTinggalDua(Player)
 		->
+		NoKartuRill is NoKartu - 1,
+		cards(Player, Hand),
+		ambilDariHand(NoKartuRill, Hand, kartu(Warna, Jenis)),
 		(
-			NoKartuRill is NoKartu - 1,
-			currentPlayer(Player),
-			cards(Player, Hand),
-			ambilDariHand(NoKartuRill, Hand, Kartu(Warna, Jenis)),
+			bisaDimainkan(Player, kartu(Warna, Jenis))
+			->
+			write(Player),
+			write(' memainkan kartu: '),
+			write(Warna),
+			write('-'),
+			write(Jenis),
+			nl,
+			jadiTop(kartu(Warna, Jenis)),
+			buangDariHand(NoKartuRill),
+			cards(Player, NewHand),
 			(
-				BisaDimainkan(Player, kartu(Warna, Jenis)) 
+				NewHand = []
 				->
-				write(Player), write('memainkan kartu: '), write(Warna), write('-'), write(Jenis), nl, efekJenis(Jenis),
-				jadiTop(kartu(Warna, Jenis)),
-				buangDariHand(NoKartuRill),
+				write(Player),
+				write(' memenangkan permainan!'),
+				nl,
+				endGame
+				;
+				efekJenis(Jenis),
 				write(Player),
 				write(' menyerukan UNI!'),
-				assertz(PlayerBilangUni(Nama)),
+				nl,
+				assertz(playerBilangUni(Player)),
 				passTurn,
-				currentPlayer(NextPlayer)
-				;
-				write('Kartu tidak bisa dimainkan, mainkan kartu lain atau ambil kartu.')
-				nl
-		).
+				currentPlayer(NextPlayer),
+				write('Giliran '),
+				write(NextPlayer),
+				write('.'),
+				nl,nl
+			),
+			!
+			;
+			write('Kartu tidak bisa dimainkan, mainkan kartu lain atau ambil kartu.'),
+			nl,
+			!,
+			fail
+		)
 		;
 		write('Kartu masih lebih dari 2, '),
 		write(Player),
-		write('mendapat 1 kartu'),
-		/* ambil kartu */
-		passTurn
-		).
+		write(' mendapat 1 kartu'),
+		nl,
+		ambilKartuUmum(Player, 1, _),
+		passTurn,
+		currentPlayer(NextPlayer),
+		write('Giliran '),
+		write(NextPlayer),
+		write('.'),
+		nl
 	).
