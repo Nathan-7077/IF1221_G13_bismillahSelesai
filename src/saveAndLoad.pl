40707f4aaf133804ede51238745c73a91e1b9b1d
zenyx7 <adityarasyid797@gmail.com>
2026-05-23 22:03:46 +0700

    feat : implement save and load function

--- src/saveAndLoad.pl
+++ src/saveAndLoad.pl
@@ -1,9 +1,28 @@
 saveGame:-
     gameStarted,
     nl,
-    
+
     write('Masukkan nama file penyimpanan: '),
-    read(FileName).
+    read(FileName),
+
+    tell(FileName),
+    listing(player),
+    listing(cards),
+    listing(points),
+    listing(currentPlayer),
+    listing(playerOrder),
+    listing(numPlayers),
+    listing(discardPile),
+    listing(gameStarted),
+    told,
+    nl,
+    
+    write('Status permainan berhasil disimpan ke '),
+    write(FileName),
+    write('.'),
+    nl,
+    
+    !.
 
 saveGame:-
     nl,
@@ -13,6 +32,28 @@ saveGame:-
 
 loadGame:-
     nl,
-    
     write('Masukkan nama file yang akan dimuat: '),
     read(FileName),
+
+    retractall(player(_)),
+    retractall(cards(_,_)),
+    retractall(points(_,_)),
+    retractall(currentPlayer(_)),
+    retractall(playerOrder(_,_)),
+    retractall(numPlayers(_)),
+    retractall(discardPile(_)),
+    retractall(gameStarted),
+    retractall(finalScore(_,_)),
+    consult(FileName),
+    nl,
+    
+    write('Status permainan berhasil dimuat dari '),
+    write(FileName),
+    write('.'),
+    nl,
+
+    currentPlayer(Player),
+    write('Melanjutkan giliran '),
+    write(Player),
+    write('.'),
+    nl,nl.
