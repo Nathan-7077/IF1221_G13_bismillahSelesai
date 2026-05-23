c001e316dce4e5e079c7fa5d3df6b831c3d5967f
zenyx7 <adityarasyid797@gmail.com>
2026-05-23 21:22:06 +0700

    feat : add save and load game functionality

---
+++ src/saveAndLoad.pl
@@ -0,0 +1,18 @@
+saveGame:-
+    gameStarted,
+    nl,
+    
+    write('Masukkan nama file penyimpanan: '),
+    read(FileName),
+
+saveGame:-
+    nl,
+    
+    write('Belum ada permainan yang berjalan.'),
+    nl.
+
+  loadGame:-
+    nl,
+    
+    write('Masukkan nama file yang akan dimuat: '),
+    read(FileName),
