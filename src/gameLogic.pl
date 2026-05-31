2874e34d2f376af40fe6b4c80e3b49d6a4125a04
Nathan-7077 <marvelnathaniel07@gmail.com>
2026-05-31 18:49:44 +0700

    Update gameLogic.pl

--- src/gameLogic.pl
+++ src/gameLogic.pl
@@ -5,13 +5,13 @@
 
 kartuValid(kartu(hitam, wild),_).
 kartuValid(kartu(hitam, wild_draw_four), _).
-%kartuValid(kartu(hitam, mimic), _). Nyiapin doang buat entar
+kartuValid(kartu(hitam, mimic), _). Nyiapin doang buat entar
 kartuValid(kartu(Color, _), kartu(Color, _)):-
     Color\=hitam.
 kartuValid(kartu(_, Type), kartu(_, Type)):-
     Type \=wild,
     Type \= wild_draw_four.
-%Type \= mimic. klo mau pake
+    Type \= mimic.
 %Cek WDF kuilangin, soalnya ternyata bisa dimainkan, cuma kalau dimainkan pemain selanjutnya bisa nantang (sebelumnya di startGame tulisannya gabisa soalnya, baru ngecek td di kelas alpro ternyata dibutuhin buat mekanik tantang)
 bisaDimainkan(Player, Card):-
     currentPlayer(Player),
